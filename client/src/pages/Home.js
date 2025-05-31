0ef40a718af6ce8c2b177f561cfc41c24d52edd1
parsaravaghi <parsadox4@gmail.com>
2025-05-31 19:57:29 +0330

    adding boxes to main page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-
+import {} from 'react-bootstrap'
 function Home()
 {
     return(
         <>
-            
+            <div className="jumbotron">
+                
+            </div>
+            <div className="p-2 container d-lg-block d-md-block d-none container-fluid">
+                <div className="row p-1 justify-content-center">
+                    <div className="box col-md-5 d-flex justify-content-center align-items-center" style={{height: "100%"}}>
+                        <img className='img-lg' src="/imgs/category.jpg" alt="" />
+                    </div>
+                    <div className="box col-md-5 d-flex justify-content-center align-items-center" style={{height: "100%"}}>
+                        <img className='img-sm' src="/imgs/category1.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
