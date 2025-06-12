b0610b940ae527012d8fc3a71fe5cae75a2ba996
parsaravaghi <parsadox4@gmail.com>
2025-06-12 23:58:12 +0330

    changing the title of user table of database (name to username) and make username unique and disable csrf validation

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->validateCsrfTokens(['*']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+        $exceptions->renderable(function (NotFoundHttpException $e ) {
+            return response()->json(['message' => '404 not found'], 404);
+        });
     })->create();
