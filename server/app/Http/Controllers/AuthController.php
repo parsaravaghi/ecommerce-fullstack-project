d466d8fa685f657f24dc2b63c2d165a55cfe7083
parsaravaghi <parsadox4@gmail.com>
2025-06-27 00:27:59 +0330

    debugging user query bug

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
             ])->first();
 
             // Match user's password and the password that we selected 
-            if(Hash::check($request->password , $user->password))
+            if($user && Hash::check($request->password , $user->password))
             {
                 // Login user
                 Auth::login($user,true);
